e0a8d3843a80a64ee1504aecb0a8a95ceb69a7e2
rusty <rwtrench@smcm.edu>
2020-05-01 17:05:28 -0400

    worked on Navbar a little more

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from polls import views
+from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('tasks/', include('tasks.urls')),
+    path('', views.homepage, name='homepage')
 ]
