c15c3f8eb7d94ac4fd1b72c2124325aca320f1e3
rusty <rwtrench@smcm.edu>
2020-05-01 10:16:28 -0400

    basic tests on a simple function

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,31 @@
+import datetime
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        should return false if published in the future
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        should return true if published in the last month
+        """
+        time = timezone.now() - datetime.timedelta(hours =23)
+        recent_question = Question(pub_date = time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        should return false if
+        """
+        time = timezone.now() - datetime.timedelta(days=1000)
+        old_question = Question(pub_date = time)
+        self.assertIs(old_question.was_published_recently(), False)
--- polls/views.py
+++ polls/views.py
@@ -5,24 +5,20 @@ from django.views import generic
 
 from .models import Choice, Question
 
-
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
+        return Question.objects.order_by('-pub_date')
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
-
 class ResultsView(generic.DetailView):
-    model = Question
     template_name = 'polls/results.html'
+    model = Question
 
 
 def vote(request, question_id):
